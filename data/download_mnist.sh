5af524fdb505a425710d1cdada46af12f490dcb3
David Heim <david.heim@uni-a.de>
2023-01-30 19:08:27 +0100

    improve download script

--- data/download_mnist.sh
+++ data/download_mnist.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-TEMP_DIR=/tmp/tmp.NkpqUvjSwS #$(mktemp -d)
+TEMP_DIR=$(mktemp -d)
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 wget -P "$TEMP_DIR" http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
@@ -8,4 +8,4 @@ wget -P "$TEMP_DIR" http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
 wget -P "$TEMP_DIR" http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
 
 for f in "$TEMP_DIR"/*.gz; do gzip -d "$f"; done
-cp "$TEMP_DIR"/* "$SCRIPT_DIR/"
+mv "$TEMP_DIR"/* "$SCRIPT_DIR/"
