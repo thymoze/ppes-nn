935577650d584bd19d8906a866abf553dd67de9b
Benedikt Bauer <benedikt.bauer@posteo.de>
2023-02-14 00:20:25 +0100

    using shared train and evaluate in main_pruning

--- src/main_pruning.cpp
+++ src/main_pruning.cpp
@@ -7,60 +7,9 @@
 #include <nn/quantization/quantization.hpp>
 #include <vector>
 
+#include "train.hpp"
+
 using tensor::Tensor;
-
-template <typename T>
-void evaluate(nn::Sequential<T>& model) {
-  std::cout << "Evaluating..." << std::endl;
-
-  auto test = nn::MnistDataset<float>("../../data", nn::MnistDataset<float>::Set::TEST, 100);
-  float correct = 0;
-  for (auto&& [input, target] : test) {
-    auto in = input.view({input.shape()[0], 28 * 28});
-    auto output_onehot = model(in);
-
-    auto output = tensor::argmax(output_onehot, 1);
-    correct +=
-        tensor::sum(output.view({input.shape()[0]}) == target.view({input.shape()[0]})).item();
-  }
-  correct /= test.count();
-  std::cout << "Test accuracy = " << correct << std::endl;
-}
-
-void train_one_epoch(nn::MnistDataset<float> mnist, nn::SGD<float>& optimizer,
-                     nn::Sequential<float>& model, int epoch) {
-  float epoch_loss = 0;
-  int i = 0;
-  for (auto&& [input, target] : mnist) {
-    optimizer.zero_grad();
-
-    auto in = input.view({input.shape()[0], 28 * 28});
-    auto output_onehot = model(in);
-
-    auto output = tensor::argmax(output_onehot, 1);
-    auto batch_correct =
-        tensor::mean(output.view({input.shape()[0]}) == target.view({input.shape()[0]}));
-
-    auto target_onehot = tensor::zeros<float>({input.shape()[0], 10});
-    for (std::size_t d = 0; d < input.shape()[0]; ++d) {
-      target_onehot(d, static_cast<std::size_t>(target(d, 0, 0))) = 1;
-    }
-    auto loss = nn::mse(output_onehot, target_onehot);
-
-    loss.backward();
-    optimizer.step();
-
-    epoch_loss += loss.item();
-    i += mnist.batch_size();
-    std::cout << "\33[2K\r" << std::setw(3) << i << ": loss = " << loss.item()
-              << " correct = " << batch_correct.item() << std::flush;
-    // model.save("../../trained_models/mnist_float_100.hpp", "mnist_model");
-  }
-  std::cout << "\33[2K\r";
-  epoch_loss = epoch_loss / mnist.size();
-
-  std::cout << "Epoch " << epoch << ": Loss = " << epoch_loss << std::endl;
-}
 
 int main() {
   auto mnist = nn::MnistDataset<float>("../../data", nn::MnistDataset<float>::Set::TRAIN, 100);
@@ -74,9 +23,7 @@ int main() {
 
   auto optimizer = nn::SGD<float>(model.params(), 0.01);
 
-  for (int epoch = 0; epoch < 3; ++epoch) {
-    train_one_epoch(mnist, optimizer, model, epoch);
-  }
+  train(model, 100, 3, 0.01);
 
   // with pruning
   auto model2 = nn::Sequential<float>();
@@ -86,13 +33,11 @@ int main() {
   model2.init();
 
   auto optimizer2 = nn::SGD<float>(model2.params(), 0.01);
-
-  train_one_epoch(mnist, optimizer2, model2, 0);
+  train(model2, 100, 1, 0.01);
 
   model2.prune(80);
 
-  train_one_epoch(mnist, optimizer2, model2, 1);
-  train_one_epoch(mnist, optimizer2, model2, 2);
+  train(model2, 100, 2, 0.01);
 
   std::cout << "Evaluation of model without pruning" << std::endl;
   evaluate<float>(model);
